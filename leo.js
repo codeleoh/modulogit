d80c332d265c3b7a04b3e8ccf3e59c6913e7aa25
Leonardo Henrique <118228587+codeleoh@users.noreply.github.com>
2022-11-21 20:02:29 -0300

    Criando o leo.js

---
+++ leo.js
@@ -0,0 +1,6 @@
+Por aqui ta tudo certo
+Fernanda é a mulher mais linda do mundo
+Pena que ela não sabe disso
+Quero poder mostrar isso a ela com as minhas palavras e atitudes
+Gostaria de ter condições de pelo menos chamar ela pra sair e dizer o que eu sinto
+Infelizmente eu sou um homem incopetente pra ter o minimo para dá um futuro pra ela
